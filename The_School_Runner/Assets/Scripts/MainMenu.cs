cbe28c10bc811f2d2138c4a97d02c68c4012d018
AYUSHMAAN GUPTA <140938372+ayushmaan30122004@users.noreply.github.com>
2025-06-01 01:10:52 +0530

    STARTMENU
    did some design changes in start menu

--- The_School_Runner/Assets/Scripts/MainMenu.cs
+++ The_School_Runner/Assets/Scripts/MainMenu.cs
@@ -5,12 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void ExitButton()
-    {
-        Application.Quit();
-        Debug.Log("Game closed");
-    }
-
     public void StartButton()
     {
         SceneManager.LoadScene(1);
